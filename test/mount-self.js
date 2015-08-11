efb32864591cf757e149a98031bda933a9938384
Kristian Mandrup <kmandrup@gmail.com>
2015-08-11 19:53:00 +0100

    update to use marooka and simplfied mounting

--- test/mount-self.js
+++ test/mount-self.js
@@ -1,11 +1,8 @@
 'use strict';
-let markoa = require('markoa');
+let marooka = require('marooka');
 let path = require('path');
 let rootPath = path.resolve(path.join(__dirname, '../'));
 
-let mounter = new markoa.AppMounter(rootPath);
-
-let apps = ['index'];
-
 // mounting multiple apps on appContainer instance
-mounter.mountApps(apps).start();
+let apps = ['index'];
+let mounter = new marooka.AppMounter(rootPath).mountApps(apps).start();
