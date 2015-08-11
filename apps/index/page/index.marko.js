ccfa13788ef73b63c0abdcf7631fa66ef1d98dc2
Kristian Mandrup <kmandrup@gmail.com>
2015-08-11 15:47:44 +0100

    add marko dependency - now hot reload works :)

--- apps/index/page/index.marko.js
+++ apps/index/page/index.marko.js
@@ -1,9 +1,10 @@
-exports.create = function(__helpers) {
+function create(__helpers) {
   var str = __helpers.s,
       empty = __helpers.e,
       notEmpty = __helpers.ne;
 
   return function render(data, out) {
-    out.w('<!DOCTYPE html> <html><head><link rel="stylesheet" type="text/css" href="semantic.min.css"><script src="semantic/dist/semantic.min.js"></script><title>Article Title</title></head><body><h1>My Article</h1><h2>HELLO JOHN</h2><p></p></body></html>');
+    out.w('<!DOCTYPE html> <html><head><link rel="stylesheet" type="text/css" href="semantic.min.css"><script src="semantic/dist/semantic.min.js"></script><title>Article Title</title></head><body><h1>My Article</h1><h2>HELLO JOHN where are you NOW? HERE</h2><p></p></body></html>');
   };
 }
+(module.exports = require("marko").c(__filename)).c(create);
